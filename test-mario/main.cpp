23d5bd53c0b1d613628ec5d8eebdd3d756139447
ChenSpike <e14096083@gs.ncku.edu.tw>
2024-04-06 21:38:59 +0800

    Add comments to main.cpp and mainwindow.cpp.

---
+++ test-mario/main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // add some changes 1
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "test-mario_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
