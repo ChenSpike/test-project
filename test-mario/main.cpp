a9165f0166d4a9645624d037d5d8c412a0a3915f
ChenSpike <e14096083@gs.ncku.edu.tw>
2024-04-06 22:40:30 +0800

    Add a comment on <QGraphicItem> lib + track more file

--- test-mario/main.cpp
+++ test-mario/main.cpp
@@ -3,14 +3,13 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-#include <QGraphicsItem>
+#include <QGraphicsItem> // Here is the new lib.
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // add some changes 1 
-    // for this
+    // add some changes again
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
