9e885e60814fe86559b64d244784993ad1bbfa98
ChenSpike <e14096083@gs.ncku.edu.tw>
2024-04-06 23:15:59 +0800

    After merge, push the latest version

--- test-mario/main.cpp
+++ test-mario/main.cpp
@@ -3,7 +3,7 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-#include <QGraphicsItem> // Here is the new lib.
+#include <QGraphicsItem> // Here is the new lib. After Merge
 
 int main(int argc, char *argv[])
 {
